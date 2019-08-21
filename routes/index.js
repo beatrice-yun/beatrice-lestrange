168dd1ab60ab6f0447b24b92b8e1340eaa228005
Béatrice DANIEL <beatricedaniel@MacBook-Pro-de-Beatrice.local>
2019-08-21 18:59:21 +0200

    structure ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+/* GET projets page */
+router.get('/projets', (req, res, next) => {
+  res.render('projets');
 });
 
 module.exports = router;
